ee3d85105358022c80c1ee234ee098fc8966d4e9
RussellWaring <russell.waring@dcmail.ca>
2021-11-17 12:51:46 -0500

    added input validation to Console class

---
+++ src/ca/durhamcollege/Console.java
@@ -0,0 +1,155 @@
+package ca.durhamcollege;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Console
+{
+    // private instance variables (members / fields)
+    private ArrayList<String> stringLog; // declare shape of variable, and initialize after in function (constructor)
+    private ArrayList<Integer> intLog; // when you instantiate an obj of 'type' this is the way
+    private ArrayList<Float> floatLog;
+    // public properties (getters / setters)
+
+    // constructors
+    public Console()
+    {
+        initialize();
+    }
+
+    // private members
+    private boolean notEmpty(ArrayList list)
+    {
+        if(list.isEmpty())
+        {
+            System.out.println("Specified Log is empty.");
+            return false;
+        }
+        return true;
+    }
+
+    // public methods
+
+    /**
+     * Initializes all input logs. If input logs already initialized - performs destructive clear.
+     */
+    public void initialize() // can be called anytime, and is destructive
+    {
+        // instantiate empty lists of various types
+        stringLog = new ArrayList<String>();
+        intLog = new ArrayList<Integer>();
+        floatLog = new ArrayList<Float>();
+    }
+
+    /**
+     * This method gets a string from the console
+     * @param prompt a friendly message to show the user
+     * @param type indicates the input type to be used
+     */
+    public void input(String prompt, InputTypes type)
+    {
+        Scanner console = new Scanner(System.in);
+        System.out.print(prompt);
+
+        try
+        {
+            switch (type)
+            {
+                case STRING:
+                    stringLog.add(console.nextLine());
+                    break;
+                case INTEGER:
+                    int intInput = console.nextInt();
+                    intLog.add(intInput);
+                    break;
+                case FLOAT:
+                    float floatInput = console.nextFloat();
+                    floatLog.add(floatInput);
+                    break;
+            }
+        }
+        catch (Exception e)
+        {
+            String message ="ERROR: Incorrect Type entered. ";
+            if(type == InputTypes.INTEGER)
+            {
+                message += "Please re-enter an Integer: ";
+                input(message, InputTypes.INTEGER);
+            }
+            else if(type == InputTypes.FLOAT)
+            {
+                message = "Please re-enter a Float: ";
+                input(message, InputTypes.FLOAT);
+            }
+        }
+    }
+
+    /**
+     * For each line in the log, outputs a line of string
+     * @param type indicates the type to be printed
+     */
+    public void printLog(InputTypes type) // no longer need for generic type, just specify what the log is
+    {
+        switch (type)
+        {
+            case STRING:
+                if(!notEmpty(stringLog))
+                {
+                    for (var line : stringLog)
+                    {
+                        System.out.println(line);
+                    }
+                }
+                break;
+            case INTEGER:
+                if(!notEmpty(intLog))
+                {
+                    for (var line : intLog)
+                    {
+                        System.out.println(line);
+                    }
+                }
+                break;
+            case FLOAT:
+                if(!notEmpty(floatLog))
+                {
+                    for (var line : floatLog)
+                    {
+                        System.out.println(line);
+                    }
+                }
+                break;
+        }
+    }
+
+    /**
+     * This function adds element to a generic array
+     * @param type indicates the log to be cleared
+     */
+    public void clear(InputTypes type)
+    {
+        switch (type)
+        {
+            case STRING:
+                stringLog.clear();
+                break;
+            case INTEGER:
+                intLog.clear();
+                break;
+            case FLOAT:
+                floatLog.clear();
+                break;
+        }
+
+    }
+
+    /**
+     * Clears all input logs (non-destructive)
+     */
+    public void clearAll()
+    {
+        stringLog.clear();
+        intLog.clear();
+        floatLog.clear();
+    }
+}
--- src/ca/durhamcollege/Main.java
+++ src/ca/durhamcollege/Main.java
@@ -4,125 +4,40 @@
  * @date    November 8th, 2021
  */
 package ca.durhamcollege;
-import java.util.Scanner;
 
 public class Main
 {
-
-    /**
-     * This method gets a string from the console
-     * @param prompt a friendly message to show the user
-     * @param object the object type to store the console input
-     * @return the value from the console
-     */
-    public static <T> T getConsoleInput(String prompt, T object)
-    {
-        Scanner console = new Scanner(System.in);
-        System.out.print(prompt);
-
-        switch (object.getClass().getSimpleName())
-        {
-            case "String":
-                return (T) console.nextLine();
-            case "Integer":
-                return (T) console.nextInt();
-            case "Float":
-                return (T) console.nextFloat();
-            default:
-                return (T) console.nextLine();
-        }
-    }
-
-    /**
-     * For each line in the log, outputs a line of string
-     * @param log string array of input lines
-     */
-    public static <T> void printLog(T[] log) throws Exception // If not getting a String, Int or Float
-    {
-        int capacity = 0;
-        String prompt = "";
-
-        if(log instanceof String[])
-        {
-            capacity = Config.NUM_OF_STRINGS;
-            prompt = "Enter your String: ";
-        }
-        else if (log instanceof Integer[])
-        {
-            log[i] = (T) new Integer(0);
-        }
-
-        for (var line:log)
-        {
-            System.out.println(line);
-        }
-    }
-
-    public static void printLog(int[] log)
-    {
-        for (var line:log)
-        {
-            System.out.println(line);
-        }
-    }
-
-    public static void printLog(float[] log)
-    {
-        for (var line:log)
-        {
-            System.out.println(line);
-        }
-    }
-
-
-    public static void buildLog(String[] log)
-    {
-        for (int i = 0; i < Config.NUM_OF_STRINGS; i++)
-        {
-            String prompt = "Enter your String: ";
-            log[i] = "";
-            log[i] = (String) getConsoleInput(prompt, log[i]);
-        }
-    }
-
-    public static void buildLog(int[] log)
-    {
-        for (int i = 0; i < Config.NUM_OF_INTEGERS; i++)
-        {
-            String prompt = "Enter your Integer: ";
-            log[i] = (int) getConsoleInput(prompt, log[i]);
-        }
-    }
-
-    public static void buildLog(float[] log)
-    {
-        for (int i = 0; i < Config.NUM_OF_FLOATS; i++)
-        {
-            String prompt = "Enter your Float: ";
-            log[i] = (float) getConsoleInput(prompt, log[i]);
-        }
-    }
-
     /**
      * Entry point for our application
      * @param args
      */
     public static void main(String[] args)
     {
-        String[] Log = new String[Config.NUM_OF_STRINGS];
-        int[] intLog = new int[Config.NUM_OF_INTEGERS];
-        float[] floatLog = new float[Config.NUM_OF_FLOATS];
+        Console console = new Console(); // instantiate class of type Console
 
-        buildLog(Log);
-        printLog(Log);
-        System.out.println(); // empty spac
-
-        buildLog(intLog);
-        printLog(intLog);
+        for (int i = 0; i < Config.NUM_OF_STRINGS; i++)
+        {
+            console.input("Enter your String: ", InputTypes.STRING);
+        }
+        console.printLog(InputTypes.STRING);
         System.out.println(); // empty space
 
-        buildLog(floatLog);
-        printLog(floatLog);
+        for (int i = 0; i < Config.NUM_OF_INTEGERS; i++)
+        {
+            console.input("Enter your Integer: ", InputTypes.INTEGER);
+        }
+        console.printLog(InputTypes.INTEGER);
+        System.out.println(); // empty space
+
+        for (int i = 0; i < Config.NUM_OF_FLOATS; i++)
+        {
+            console.input("Enter your Float: ", InputTypes.FLOAT);
+        }
+        console.printLog(InputTypes.FLOAT);
+        System.out.println(); // empty space
+
+        console.clearAll();
+        console.printLog(InputTypes.INTEGER);
         System.out.println(); // empty space
     }
 }
